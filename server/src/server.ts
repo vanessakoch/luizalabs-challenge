4a53afa7e9cc3bbbd908799086f9fa418b6c579c
Vanessa <vahnkoch@gmail.com>
2021-08-15 17:33:23 -0300

    Tratamento de erros e comentários

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import "reflect-metadata";
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import swaggerUI from 'swagger-ui-express';
+import "express-async-errors";
 
 import { routes } from './routes';
+import { AppError } from './shared/errors/AppError';
 import swaggerFile from './swagger.json';
 import mongoose from 'mongoose';
 
@@ -20,5 +22,18 @@ app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error ${err.message}`
+  })
+})
 
 app.listen(3333);
